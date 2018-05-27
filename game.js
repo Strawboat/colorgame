8f49bee682df3710f79af99338dd830eea5e0771
CHECK <zevirtual720@gmail.com>
2018-05-27 23:34:48 +0530

    Added Most Functions

--- game.js
+++ game.js
@@ -1,16 +1,8 @@
-var colors = [
-			 "rgb(255, 0, 0)",
-			 "rgb(255, 255, 199)",
-			 "rgb(255, 0, 255)",
-			 "rgb(255, 255, 0)",
-			 "rgb(255, 0, 255)",
-			 "rgb(255, 255, 255)",
-			 
-			 ]
+var colors = generate(6);
 
 var div_color = document.querySelectorAll(".alpha_div");
 var pickedColor = pickColor();
-
+h1ee = document.querySelector("h1")
 colorDisplay = document.querySelector(".colorpicked")
 colorDisplay.innerHTML = pickedColor
 //Logic for displaying color
@@ -22,6 +14,7 @@ for(var i = 0; i < div_color.length ; i++){
 		if(pickcolor == pickedColor){
 			document.querySelector(".wrong").innerHTML = "Right";
 			changeColors(pickedColor);
+			h1ee.style.backgroundColor = pickedColor;
 		}else{
 			this.style.backgroundColor = "#285481";
 			document.querySelector(".wrong").innerHTML = "Try Again"
@@ -40,3 +33,17 @@ function pickColor() {
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
 }
+ function generate(num){
+ 	var earr = []
+ 	for(var i = 0;i< num;i++){
+ 		earr.push(randomColor());
+ 	}
+ 	return earr;
+ }
+
+ function randomColor(){
+ 	var r = Math.floor(Math.random()*256);
+ 	var g = Math.floor(Math.random()*256);
+ 	var y = Math.floor(Math.random()*256);
+ 	return "rgb(" + r + ", " + g + ", " + y + ")";
+ }
